c688e1d9e48101533991339e7fb29c2f52ae083a
Sergey Tsepilov <stpilovdev@gmail.com>
2025-06-20 12:27:39 +0500

    index

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-document.getElementById('year').textContent = new Date().getFullYear();
+const videos = document.querySelectorAll('.video__element');
+videos.forEach((video) => {
+  video.setAttribute('controlsList', 'nodownload');
+});
+
+const year = document.getElementById('year');
+if (year) {
+  year.textContent = new Date().getFullYear();
+}
